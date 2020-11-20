33c6b043f9477c367ab7bd637c991dcbbe2044bd
Eric Liu <ericliu4943@gmail.com>
2020-11-19 22:45:45 -0800

    Initial Commit

---
+++ BucketList/BucketListApp.swift
@@ -0,0 +1,17 @@
+//
+//  BucketListApp.swift
+//  BucketList
+//
+//  Created by Eric Liu on 11/19/20.
+//
+
+import SwiftUI
+
+@main
+struct BucketListApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
